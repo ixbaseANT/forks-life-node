5fe0ba272ce9947776900c14e35b0ce5d54c8337
ixbaseANT <98494470+ixbaseANT@users.noreply.github.com>
2025-09-09 16:52:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo "📦 Установка PHP, SQLite и Python-зависимостей..."
 sudo apt install -y php php-sqlite3 php-fpm php-curl php-xml sqlite3 python3 python3-pip
 sudo apt install python3.11-venv -y
 
-echo "🐍 Создание Python venv..."
+echo "🐍 Создание Python venv от имени www-data..."
 sudo -u www-data bash -c '
 cd /var/www/html/fork
 python3 -m venv venv
